ea1bd2ccdf4cf8d3fe0ab1664e6a6392b65c55d3
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 01:52:01 +0530

    121 : Best Time to Buy and Sell Stocks

---
+++ Day 1/Best Time to Buy and Sell Stocks.cpp
@@ -0,0 +1,23 @@
+/*
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        int ans = 0;
+
+        int mini = INT_MAX;
+        for(int i=0; i<n; i++){
+            mini = min(mini,prices[i]);
+            ans = max(ans,prices[i]-mini);
+        }
+
+        return ans;
+
+    }
+};
