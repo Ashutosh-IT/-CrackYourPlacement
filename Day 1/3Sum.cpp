e914580032e142f6a8c19f3f519e06821147111f
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 03:18:58 +0530

    15 : 3Sum.cpp

--- Day 1/3Sum.cpp
+++ Day 1/3Sum.cpp
@@ -32,3 +32,46 @@ public:
         return ans; 
     }
 };
+
+
+// without using set :
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0; i<=n-3; i++){
+
+            // skips duplicates for 1st element
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int s=i+1;
+            int e=n-1;
+
+            while(s<e){
+                int sum = nums[i] + nums[s] + nums[e];
+                if(sum == 0){
+                    ans.push_back({nums[i],nums[s],nums[e]});
+                    s++;
+                    e--;
+
+                    // skips duplicate for 2nd element
+                    while(s<e && nums[s] == nums[s-1]) s++;
+
+                    // skips duplicates for 3rd element
+                    while(s<e && nums[e] == nums[e+1]) e--;
+                    
+                }
+                else if(sum > 0) e--;
+                else s++;
+            }
+        }
+
+        return ans; 
+    }
+};
