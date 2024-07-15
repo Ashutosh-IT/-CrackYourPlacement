81dd42f46f4af5ff35919e8cc02782d577235f92
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 02:10:27 +0530

    1 : Two Sum

---
+++ Day 1/Two Sum.cpp
@@ -0,0 +1,22 @@
+/*
+
+https://leetcode.com/problems/two-sum/description/
+  
+*/
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<n; i++){
+            if(mp.find(target-nums[i]) != mp.end())
+            return {i,mp[target-nums[i]]};
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
