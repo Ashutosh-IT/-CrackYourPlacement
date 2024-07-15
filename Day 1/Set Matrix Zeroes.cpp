54fb8733dc51f0bd873dd768c5c6d2c07f046bfd
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 01:37:46 +0530

    73 : Set Matrix Zeroes

---
+++ Day 1/Set Matrix Zeroes.cpp
@@ -0,0 +1,60 @@
+/*
+
+https://leetcode.com/problems/set-matrix-zeroes/description/
+
+*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int row1 = 1;
+        int col1 = 1;
+
+        for(int i=0; i<n; i++)
+        if(matrix[i][0] == 0){
+            col1 = 0;
+            break;
+        }
+        
+
+        for(int j=0; j<m; j++)
+        if(matrix[0][j] == 0){
+            row1 = 0;
+            break;
+        } 
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+
+                if(matrix[i][j] == 0){
+
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+
+                }
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                matrix[i][j] = 0;
+
+            }
+        }
+
+        if(!col1)
+        for(int i=0; i<n; i++)
+        matrix[i][0] = 0;
+        
+        if(!row1)
+        for(int j=0; j<m; j++)
+        matrix[0][j] = 0;
+
+    }
+};
