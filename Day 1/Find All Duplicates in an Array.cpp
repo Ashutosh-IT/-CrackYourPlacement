be1b99940741b7b0733c9df7a0acbd9fb2e988e0
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 02:32:58 +0530

    442 : Find All Duplicates in an Array

---
+++ Day 1/Find All Duplicates in an Array.cpp
@@ -0,0 +1,24 @@
+/*
+
+https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+*/
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        
+        vector<int> ans;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            int num = abs(nums[i]);
+            if(nums[num-1] < 0)
+            ans.push_back(num);
+            else nums[num-1] *= -1;
+        }
+
+        return ans;
+
+    }
+};
