cd45c58a4b996e00831eae4888949f15216e0114
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-16 12:01:59 +0530

    55 : Jump Game.cpp

--- Day 1/Jump Game.cpp
+++ Day 1/Jump Game.cpp
@@ -4,6 +4,9 @@ https://leetcode.com/problems/jump-game/description/
 
 */
 
+
+// TC -> O(N^2) 
+// SC -> O(N)
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -24,4 +27,28 @@ public:
         }
         return dp[n-1];
     }
+};
+
+
+
+// TC -> O(N) 
+// SC -> O(1)
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return true;
+
+        int maxIdx = nums[0];
+        for(int i=0; i<n; i++){
+
+            if(maxIdx >= n-1) return true;
+            if(maxIdx == i && nums[i] == 0) return false;
+            if(nums[i] + i > maxIdx) maxIdx = i + nums[i];
+
+        }
+
+        return true;
+    }
+
 };
