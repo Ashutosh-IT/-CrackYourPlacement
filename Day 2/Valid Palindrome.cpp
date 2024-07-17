8c46c4a360d4c95d5788ac1cb9d1c692c640f9f9
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-17 23:10:39 +0530

    125 : Valid Palindrome

---
+++ Day 2/Valid Palindrome.cpp
@@ -0,0 +1,30 @@
+/*
+
+https://leetcode.com/problems/valid-palindrome/description/
+
+*/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+
+            if(i>=j) continue;
+            
+            if(tolower(s[i]) != tolower(s[j]))
+            return false;
+
+            i++;
+            j--; 
+        }
+
+        return true;
+    }
+};
