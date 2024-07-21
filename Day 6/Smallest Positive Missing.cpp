3d2617d34b40a66f10473b2e4dad99ae63765525
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-21 23:24:04 +0530

    Smallest Positive Missing.cpp

--- Day 6/Smallest Positive Missing.cpp
+++ Day 6/Smallest Positive Missing.cpp
@@ -33,3 +33,30 @@ class Solution
         
     } 
 };
+
+
+
+// Another Approach : Rohit negi - store two numbers in one place approach (negative waala)
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            if(nums[i]<=0) 
+            nums[i] = n+1;
+        }
+
+        for(int i=0; i<n; i++){
+            if(abs(nums[i])<=n)
+            nums[abs(nums[i])-1] = nums[abs(nums[i])-1]<0 ? nums[abs(nums[i])-1] : -nums[abs(nums[i])-1];
+        }
+
+        for(int i=0; i<n; i++){
+            if(nums[i]>0) return i+1;
+        }
+
+        return n+1;
+    }
+};
