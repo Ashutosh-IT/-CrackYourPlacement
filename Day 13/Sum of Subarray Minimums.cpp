888b90cfc840b58004cd5d3ead015f70a2c74f6b
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-28 21:26:08 +0530

    907 : Sum of Subarray Minimums

--- Day 13/Sum of Subarray Minimums.cpp
+++ Day 13/Sum of Subarray Minimums.cpp
@@ -26,3 +26,61 @@ public:
     }
 };
 
+
+
+// Better Approach - O(N)
+class Solution {
+public:
+
+    int mod = 1e9+7;
+
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> right(n,n);
+        vector<int> left(n,-1);
+
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            if(st.empty())
+            st.push(i);
+
+            else if(arr[st.top()] <= arr[i])
+            st.push(i);
+
+            else{
+                right[st.top()] = i;
+                st.pop();
+                i--;
+            }
+        }
+
+        while(!st.empty()) st.pop();
+
+        for(int i=n-1; i>=0; i--){
+            if(st.empty())
+            st.push(i);
+
+            else if(arr[st.top()] < arr[i])
+            st.push(i);
+
+            else{
+                left[st.top()] = i;
+                st.pop();
+                i++;
+            }
+        }
+
+
+        long long sum = 0;
+        for(int i=0; i<n; i++){
+            long long ls = i-left[i];
+            long long rs = right[i]-i;
+
+            long long num = ls*rs;
+            sum += (num*arr[i]);
+        }
+
+        return sum%mod;
+    }
+};
+
