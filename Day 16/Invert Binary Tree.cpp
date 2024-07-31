6dd376b9c58993a2072ee2a6864d0da925fa215c
Ashutosh Kumar <127471296+Ashutosh-IT@users.noreply.github.com>
2024-07-31 23:01:14 +0530

    226 : Invert Binary Tree.

---
+++ Day 16/Invert Binary Tree.cpp
@@ -0,0 +1,19 @@
+/*
+
+https://leetcode.com/problems/invert-binary-tree/
+
+*/
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        swap(root->left,root->right);
+        return root;
+    }
+};
